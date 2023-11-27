af6669d1113dfdd661a57ccf20dc0e07baf147dc
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-26 23:26:42 -0600

    added location-search.predefined,geolocation-btn

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+document.getElementById('geolocation-btn').addEventListener('click', () => {
+    
+});
+
+document.getElementById('predefined-locations').addEventListener('change', (event) => {
+    
+});
+
+document.getElementById('location-search').addEventListener('input', (event) => {
+
+});
+
+function fetchSunriseSunset(lat, lng) {
+
+}
+
